ad3814b6b5130d6e25a8ea15a4045afac71a7d53
AndreitaBravo17 <100541544+AndreitaBravo17@users.noreply.github.com>
2023-07-21 14:44:54 -0500

    Delete especialidadModel.js

--- backend/app/models/especialidadModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const especialidadSchema = new mongoose.Schema({
-  especialidad: { type: String, required: true },
-});
-
-const Especialidad = mongoose.model('especialidades', especialidadSchema, 'especialidades');
-
-module.exports = Especialidad;
