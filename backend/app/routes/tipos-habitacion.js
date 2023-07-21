660686219e6d43b95737027d9042d07f4849ee2d
AndreitaBravo17 <100541544+AndreitaBravo17@users.noreply.github.com>
2023-07-21 14:47:38 -0500

    Delete tipos-habitacion.js

--- backend/app/routes/tipos-habitacion.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router()
-const controller = require('../controller/tiposHabitacion');
-const { validateToken } = require("../middleware/validateToken")
-router.get("", validateToken, controller.obtenerTodosTiposHabitacion) 
-
-module.exports = { router }
