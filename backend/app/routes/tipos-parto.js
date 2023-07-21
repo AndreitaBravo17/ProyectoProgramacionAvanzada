2721bbc5b7c4d2a1f4fec562b2dbd6a5ebb5d67d
AndreitaBravo17 <100541544+AndreitaBravo17@users.noreply.github.com>
2023-07-21 14:47:40 -0500

    Delete tipos-parto.js

--- backend/app/routes/tipos-parto.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router()
-const controller = require('../controller/tiposPartoController');
-const { validateToken } = require("../middleware/validateToken")
-router.get("", validateToken, controller.obtenerTodosTiposParto) 
-
-module.exports = { router }
