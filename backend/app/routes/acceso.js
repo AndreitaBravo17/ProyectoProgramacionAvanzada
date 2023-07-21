9e97ee350ff7c581e4a27c8f9e301241d5e4b3b7
AndreitaBravo17 <100541544+AndreitaBravo17@users.noreply.github.com>
2023-07-21 14:46:33 -0500

    Delete acceso.js

--- backend/app/routes/acceso.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router()
-const controller = require('../controller/accesoController');
-
-router.post("/auth", controller.obtenerAcceso) 
-
-module.exports = { router }
