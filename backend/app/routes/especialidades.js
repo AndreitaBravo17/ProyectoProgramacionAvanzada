d061fc5aaf7ede0610fe3ac57812d3aed49c3548
AndreitaBravo17 <100541544+AndreitaBravo17@users.noreply.github.com>
2023-07-21 14:46:40 -0500

    Delete especialidades.js

--- backend/app/routes/especialidades.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router()
-const controller = require('../controller/especialidadController');
-const { validateToken } = require("../middleware/validateToken")
-router.get("", validateToken, controller.obtenerEspecialidades) 
-
-module.exports = { router }
